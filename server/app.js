bee80769d80f3b7e11d31b8d3b8530b513eee3fa
achimbaggins <achim_baggins@yahoo.com>
2017-09-27 13:15:43 +0700

    ready to google cloud

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/travel', {
+mongoose.connect('mongodb://achim:mQ8sOpOikNKXTjWt@cluster0-shard-00-00-j6d3u.mongodb.net:27017,cluster0-shard-00-01-j6d3u.mongodb.net:27017,cluster0-shard-00-02-j6d3u.mongodb.net:27017/govision?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', {
   useMongoClient: true,
   promiseLibrary: global.Promise
 });
